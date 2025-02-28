7f90530b2f1345a44bca974775d38622b6b9df75
HirokenOvo <526420278@qq.com>
2025-02-28 22:10:24 +0800

    completed the implementation of gather, small data is slow.

--- test/gather.py
+++ test/gather.py
@@ -9,17 +9,21 @@ import performance
 import sys
 import os
 
-lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.././build/lib/libmy_library.so')
+lib_path = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '.././build/lib/libmy_library.so')
 lib = ctypes.CDLL(lib_path)
+
 
 def gather(rank, axis, inputTensor, indexTensor):
     indices = [slice(None)] * rank
     indices[axis] = indexTensor
     outTensor = inputTensor[tuple(indices)]
     return outTensor
+
+
 def test(inputShape, indexShape, axis, test_dtype, device):
     print(
-        f"Testing Softmax on {device} with x_shape:{inputShape} , indice_shape:{indexShape}, axis:{axis} ,dtype:{test_dtype}"
+        f"Testing Gather on {device} with x_shape:{inputShape} , indice_shape:{indexShape}, axis:{axis} ,dtype:{test_dtype}"
     )
     inputTensor = torch.rand(inputShape, device=device, dtype=test_dtype)
 
@@ -27,23 +31,61 @@ def test(inputShape, indexShape, axis, test_dtype, device):
     indexTensor = torch.from_numpy(index).to(torch.int64).to(device)
 
     rank = len(inputShape)
-    outTensor = gather(rank, axis, inputTensor, indexTensor)#
+    outTensor = gather(rank, axis, inputTensor, indexTensor)
 
     Q_output = torch.zeros(outTensor.shape, device=device, dtype=test_dtype)
-    input_ptr = ctypes.cast(inputTensor.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
-    index_ptr = ctypes.cast(indexTensor.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
-    output_ptr = ctypes.cast(Q_output.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
+    input_ptr = ctypes.cast(inputTensor.data_ptr(),
+                            ctypes.POINTER(ctypes.c_void_p))
+    index_ptr = ctypes.cast(indexTensor.data_ptr(),
+                            ctypes.POINTER(ctypes.c_void_p))
+    output_ptr = ctypes.cast(
+        Q_output.data_ptr(), ctypes.POINTER(ctypes.c_void_p))
+    input_shape_ptr = ctypes.cast(
+        (ctypes.c_int * len(inputShape))(*inputShape), ctypes.POINTER(ctypes.c_int))
+    index_shape_ptr = ctypes.cast(
+        (ctypes.c_int * len(inputShape))(*indexShape), ctypes.POINTER(ctypes.c_int))
+    index_shape_rank = len(indexShape)
 
     if test_dtype == torch.float32:
         if device == "cuda":
-            torch_gather_time = performance.CudaProfile((gather, (rank, axis, inputTensor, indexTensor)))
-            custom_gather_time = 0
+            torch_gather_time = performance.CudaProfile(
+                (gather, (rank, axis, inputTensor, indexTensor)))
+            lib.gather_nv_f32.argtypes = [
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.c_int,
+                ctypes.POINTER(ctypes.c_int),
+                ctypes.c_int,
+                ctypes.POINTER(ctypes.c_int),
+                ctypes.c_int
+            ]
+            custom_gather_time = performance.CudaProfile(
+                (lib.gather_nv_f32, (input_ptr, index_ptr, output_ptr, axis, input_shape_ptr, rank, index_shape_ptr, index_shape_rank)))
     if test_dtype == torch.float16:
         if device == "cuda":
-            torch_gather_time = performance.CudaProfile((gather, (rank, axis, inputTensor, indexTensor)))
-            custom_gather_time = 0
+            torch_gather_time = performance.CudaProfile(
+                (gather, (rank, axis, inputTensor, indexTensor)))
+            lib.gather_nv_f16.argtypes = [
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.POINTER(ctypes.c_void_p),
+                ctypes.c_int,
+                ctypes.POINTER(ctypes.c_int),
+                ctypes.c_int,
+                ctypes.POINTER(ctypes.c_int),
+                ctypes.c_int
+            ]
+            custom_gather_time = performance.CudaProfile(
+                (lib.gather_nv_f16, (input_ptr, index_ptr, output_ptr, axis, input_shape_ptr, rank, index_shape_ptr, index_shape_rank)))
     performance.logBenchmark(torch_gather_time, custom_gather_time)
-
+    # print(inputTensor)
+    # print("===========")
+    # print(indexTensor)
+    # print("===========")
+    # print(outTensor)
+    # print("===========")
+    # print(Q_output)
     tmpa = outTensor.to('cpu').numpy().flatten()
     tmpb = Q_output.to('cpu').numpy().flatten()
 
@@ -51,30 +93,35 @@ def test(inputShape, indexShape, axis, test_dtype, device):
 
     rtol = atol / max(abs(tmpb) + 1e-8)
 
+    print("absolute error:%.4e" % (atol))
+    print("relative error:%.4e" % (rtol))
 
-    print("absolute error:%.4e"%(atol))
-    print("relative error:%.4e"%(rtol))
-parser = argparse.ArgumentParser(description="Test softmax on different devices.")
-parser.add_argument('--device', choices=['cpu', 'cuda', 'mlu'], required=True, help="Device to run the tests on.")
-args = parser.parse_args()    
+
+parser = argparse.ArgumentParser(
+    description="Test Gather on different devices.")
+parser.add_argument('--device', choices=['cpu', 'cuda', 'mlu'],
+                    required=True, help="Device to run the tests on.")
+args = parser.parse_args()
 test_cases = [
-        # inputShape , indexShape, axis, test_dtype, device
-        ((3, 2), (2, 2), 0, torch.float32, "cuda"),
-        ((3, 2), (1, 2), 1, torch.float32, "cuda"),
-        ((50257, 768), (16, 1024), 0, torch.float32, "cuda"),
+    # inputShape , indexShape, axis, test_dtype, device
+    ((3, 2), (2, 2), 0, torch.float32, "cuda"),
+    ((3, 2), (1, 2), 1, torch.float32, "cuda"),
+    ((3, 2, 3), (1, 2, 1), 1, torch.float32, "cuda"),
+    ((3, 2, 3), (1, 2, 1), 1, torch.float32, "cuda"),
+    ((50257, 768), (16, 1024), 0, torch.float32, "cuda"),
 
-        ((3, 2), (2, 2), 0, torch.float16, "cuda"),
-        ((3, 2), (1, 2), 1, torch.float16, "cuda"),
-        ((50257, 768), (16, 1024), 0, torch.float16, "cuda"),
-         
+    ((3, 2), (2, 2), 0, torch.float16, "cuda"),
+    ((3, 2), (1, 2), 1, torch.float16, "cuda"),
+    ((50257, 768), (16, 1024), 0, torch.float16, "cuda"),
+
 ]
 filtered_test_cases = [
-    (inputShape , indexShape, axis, test_dtype, device)
-    for inputShape , indexShape, axis, test_dtype, device in test_cases
+    (inputShape, indexShape, axis, test_dtype, device)
+    for inputShape, indexShape, axis, test_dtype, device in test_cases
     if device == args.device
 ]
 if args.device == 'mlu':
     import torch_mlu
 # 执行过滤后的测试用例
-for inputShape , indexShape, axis, test_dtype, device in filtered_test_cases:
-    test(inputShape , indexShape, axis, test_dtype, device)
+for inputShape, indexShape, axis, test_dtype, device in filtered_test_cases:
+    test(inputShape, indexShape, axis, test_dtype, device)
